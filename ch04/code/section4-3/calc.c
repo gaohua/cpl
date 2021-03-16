df4f341290073e933f44610a7fde7e38f28d64ae
gaohuaid <gaohuacode@gmail.com>
2021-03-16 13:31:14 +0800

    add section 4-3 example code

---
+++ ch04/code/section4-3/calc.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define MAXOP   100
+#define NUMBER  '0'
+#define MAXVALUE 100    /* maximum depth of val stack */
+#define BUFSIZE 100
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type= getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break; 
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknow command %s\n",s);
+            break;
+        }
+    }
+    return 0;
+}
+
+int sp = 0;             /* next free stack position */
+double val[MAXVALUE];   /* value stack */
+
+/* push: push f onto value stack */
+void push(double f)
+{
+    if(sp < MAXVALUE)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n",f); 
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+    if(sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+    int i, c;
+
+    while((s[0] = c = getchar()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if(!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if(isdigit(c))
+        while(isdigit(s[++i] = c = getchar()))
+            ;
+    if(c == '.')
+        while(isdigit(s[++i] = c = getchar()))
+            ;
+    s[i] = '\0';
+    if(c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if(bufp >= BUFSIZE)
+    {
+        printf("ungetch: too many characters\n");
+    }
+    else
+    {
+        buf[bufp++] = c;
+    }
+}
